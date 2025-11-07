f9645f618de28373303ca00f20cd98227d1dd574
nassipkaliv <nassipkaliv@gmail.com>
2025-11-07 16:56:30 +0500

    depenencies

---
+++ src/screens/profileScreen.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native';
+
+
+const HomeScreen = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-slate-900">
+      <Text className="text-white text-xl font-bold">
+        It is profileScreen
+      </Text>
+    </View>
+  )
+}
+
+export default HomeScreen;
